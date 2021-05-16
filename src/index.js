73854cc744547b0a4bec25796457c1fb7a6a2e7d
Inna <i.yemelyanova@gmail.com>
2021-05-16 08:57:48 +0300

    lesson 1

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+const messages = ['hello']
+
+const sendMessage = () => {
+  messages.push('new message')
+  render()
+}
+
 const Message = ({ message }) => {
   return (
     <React.Fragment>
       <h1>{message}</h1>
-      <p>&quot;qwd&quot;</p>
     </React.Fragment>
   )
 }
 
 const MessageField = ({ messages }) => {
-  return messages.map((message, index) => (
-    <Message message={message} key={index} isVisible={true} />
-  ))
+  return (
+    <>
+      <button onClick={sendMessage}>Send</button>
+      {messages.map((message, index) => (
+        <Message message={message} key={index} isVisible={true} />
+      ))}
+    </>
+  )
 }
 
-ReactDOM.render(
-  <MessageField messages={["test1"]} />,
-  document.getElementById("root"),
-)
+const render = () => {
+  ReactDOM.render(
+    <MessageField messages={messages} />,
+    document.getElementById("root"),
+  )
+}
+
+render()
